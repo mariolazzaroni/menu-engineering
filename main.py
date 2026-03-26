6be588509d5ec82b6e1dfa134f50ecf5edd5843c
Mario Lazzaroni <mariolazzaroni@MacBook-di-Mario.local>
2026-03-26 17:31:28 +0100

    new variable name

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
-dataset = pd.read_csv('data_set_bar.csv')
-dataset.info
-dataset.isnull().sum()
-dataset.duplicated().sum()
-dataset = dataset.drop_duplicates()
-dataset.to_csv('cleaned_datset_bar.csv', index = False)
+
+vendite_bar = pd.read_csv('data_set_bar.csv')
+vendite_bar.info
+vendite_bar.isnull().sum()
+vendite_bar.duplicated().sum()
+vendite_bar = vendite_bar.drop_duplicates()
+vendite_bar.to_csv('cleaned_datset_bar.csv', index = False)
