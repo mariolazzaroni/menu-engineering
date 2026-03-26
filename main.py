ee8f27de122c6922b7f85cd7c40dc308d6e09b8d
Mario Lazzaroni <mariolazzaroni@MacBook-di-Mario.local>
2026-03-26 17:28:17 +0100

    datacleaning

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import pandas as pd
-
+dataset = pd.read_csv('data_set_bar.csv')
+dataset.info
+dataset.isnull().sum()
+dataset.duplicated().sum()
+dataset = dataset.drop_duplicates()
+dataset.to_csv('cleaned_datset_bar.csv', index = False)
